b0410f5001d99cbdb4422482590c9250fa186c94
Martin Faartoft <martin.faartoft@gmail.com>
2013-05-01 15:56:01 +0200

    bootstrapped

--- script/main.js
+++ script/main.js
@@ -2,26 +2,23 @@ $(document).ready(function() {
 	ko.applyBindings(new LectureListViewModel());
 });
 
+var rowHeight = 42;
+var colWidth = 136;
+var rowMargin = 5;
+var timeColWidth = 56;
 
 function Lecture(data) {
     this.title = ko.observable(data.title);
-    this.weekday = data.weekday * 120; //number between 0-7
-    this.startTime = (data.startTime - 7) * 25; //int
-    this.duration = data.duration * 25; //int
+    this.left = (data.weekday) * colWidth + timeColWidth; //number between 0-7
+    this.top = (data.startTime - 7) * rowHeight - rowMargin; //int
+    this.height = data.duration * rowHeight - 1; //int
 }
 
 function LectureListViewModel() {
-    // Data
     var self = this;
     self.lectures = ko.observableArray([]);
-    //self.selectedCourses = ko.observableArray([]);
 
-    self.lectures.push(new Lecture({title: "MDD", weekday: 1, startTime: 8, duration: 2}));
-    self.lectures.push(new Lecture({title: "Discrete Math", weekday: 3, startTime: 14, duration: 1}));
-    // Operations
-    // self.addCourse = function() {
-    //     self.courses.push(new Course({ title: this.newTaskText() }));
-    //     self.newTaskText("");
-    // };
-    // self.removeTask = function(task) { self.tasks.remove(task) };
+    self.lectures.push(new Lecture({title: "MDD", weekday: 0, startTime: 8, duration: 2}));
+    self.lectures.push(new Lecture({title: "Discrete Math", weekday: 1, startTime: 8, duration: 1}));
+    self.lectures.push(new Lecture({title: "Discrete Math Ex", weekday: 2, startTime: 8, duration: 4}));
 }
