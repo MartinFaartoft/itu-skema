fd98108fcd97a6eb9f07bfc54eacdf2bb67bbcd3
Martin Faartoft <martin.faartoft@gmail.com>
2013-05-24 09:51:41 +0200

    updating udpdate.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/MartinFaartoft/itu-skema.git
+rm -rf public_html/*
 mv itu-skema/index.html public_html/
-mv itu-skema/css/ public_html/css
-mv itu-skema/script/ public_html/script
-rm -rf itu-skema
+mv itu-skema/css/ public_html/
+mv itu-skema/script/ public_html/
+rm -rf itu-skema/
